9860a32b96eaf2eb2d925dfc2ac2db7335aa267e
t-drape <thebatreview1@gmail.com>
2024-07-17 10:59:13 -0700

    Finish all pseudocode/comments

---
+++ picker.rb
@@ -0,0 +1,33 @@
+# BEST_PRICE = SELL_PRICE - BUY_PRICE
+def picker(prices)
+# Step 1:
+  # Get input array of stock prices from user
+# Step 2:
+  # Create variable to hold current best price to buy
+# Step 3:
+  # Create variable to hold current best price to sell
+# Step 3: 
+  # Loop through array
+# Step 4:
+  # While in loop
+# Step 4a:
+  # Compare current value of variable to current index of array for highest sell price
+# Step 4aa:
+  # If current index value is higher, assign index value to variable, 
+# Step 4ab:
+  # Otherwise continue
+# Step 4b:
+  # Compare current value of variable to current index of array for lowest buy price
+# Step 4ba:
+  # If current index value is lower, assign index value to variable
+# Step 4bb:
+  # Otherwise continue
+
+# Step 5:
+  # Implement functionality so sell can only be after buy
+
+# Step 5a:
+  # Best price formula = sell_price - buy_price
+
+# Step 6:
+  # Return buy_price, sell_price in new array
